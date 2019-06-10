90da00551fcda5b56300e9f3dc409141fd94eb7a
sam-it <samit@MyMac.local>
2019-06-10 00:22:36 -0500

    updated..

--- samsetup.sh
+++ samsetup.sh
@@ -2,10 +2,11 @@
 clear
 TFLINK=https://releases.hashicorp.com/terraform/0.12.1/terraform_0.12.1_linux_amd64.zip
 TFVERS=12
+
 #root only!
 echo "Checking to see if you are root"
 if [ "$(whoami)" != 'root' ]; then
-	echo "You are not root, please f off!"
+	echo "You are not root; the door is that way!"
 	exit 1
 else
     	echo "Ok, you are root! Proceeding.."
